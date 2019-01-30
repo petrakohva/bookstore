b2a1857676386b7fe4ea3aa5c2a085b5f836d142
Petra Kohva <petra.kohva@myy.haaga-helia.fi>
2019-01-30 10:55:02 +0200

    projekti aloitettu

---
+++ bookstoremavenproject/src/main/java/hh/swd20/bookstoremavenproject/BookstoremavenprojectApplication.java
@@ -0,0 +1,14 @@
+package hh.swd20.bookstoremavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremavenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremavenprojectApplication.class, args);
+	}
+
+}
+
---
+++ bookstoremavenproject/src/test/java/hh/swd20/bookstoremavenproject/BookstoremavenprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package hh.swd20.bookstoremavenproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookstoremavenprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
