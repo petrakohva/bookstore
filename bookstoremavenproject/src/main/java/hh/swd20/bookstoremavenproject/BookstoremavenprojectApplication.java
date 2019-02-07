9c0a2dbd6ac71190ef69fc135d4404c211a92819
Petra Kohva <petra.kohva@myy.haaga-helia.fi>
2019-02-07 17:55:26 +0200

    JPA ja database lisätty

--- bookstoremavenproject/src/main/java/hh/swd20/bookstoremavenproject/BookstoremavenprojectApplication.java
+++ bookstoremavenproject/src/main/java/hh/swd20/bookstoremavenproject/BookstoremavenprojectApplication.java
@@ -1,13 +1,35 @@
 package hh.swd20.bookstoremavenproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstoremavenproject.domain.Book;
+import hh.swd20.bookstoremavenproject.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoremavenprojectApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoremavenprojectApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoremavenprojectApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save books");
+			repository.save(new Book("Harry Potter and the Philosophers Stone", "J. K. Rowling", 1997, "a13j533", 19.99));
+			repository.save(new Book("Harry Potter and the Chamber of Secrets", "J. K. Rowling", 1999, "bh7362k", 19.99));
+			
+			log.info("find books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 
 }
---
+++ bookstoremavenproject/src/main/java/hh/swd20/bookstoremavenproject/domain/BookRepository.java
@@ -0,0 +1,9 @@
+package hh.swd20.bookstoremavenproject.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+	
+	
+}
