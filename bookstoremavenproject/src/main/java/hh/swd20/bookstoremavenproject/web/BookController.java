b65c58809e64b92ff2bbd9c22b51486dfecba464
Petra Kohva <petra.kohva@myy.haaga-helia.fi>
2019-03-05 20:27:32 +0200

    Lisätty REST

--- bookstoremavenproject/src/main/java/hh/swd20/bookstoremavenproject/web/BookController.java
+++ bookstoremavenproject/src/main/java/hh/swd20/bookstoremavenproject/web/BookController.java
@@ -1,11 +1,15 @@
 package hh.swd20.bookstoremavenproject.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.bookstoremavenproject.domain.Book;
 import hh.swd20.bookstoremavenproject.domain.BookRepository;
@@ -14,15 +18,25 @@ import hh.swd20.bookstoremavenproject.domain.GenreRepository;
 @Controller
 public class BookController {
 	@Autowired
-	private BookRepository repository;
+	private BookRepository brepository;
 	
 	@Autowired
 	private GenreRepository grepository;
 	
 	@RequestMapping(value="/booklist", method=RequestMethod.GET)
 	public String bookList(Model model) {
-		model.addAttribute("books", repository.findAll());
+		model.addAttribute("books", brepository.findAll());
 		return "booklist";
+	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) brepository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){
+		return brepository.findById(bookId);
 	}
 	
 	@RequestMapping(value="/add")
@@ -34,19 +48,19 @@ public class BookController {
 	
 	@RequestMapping(value="/save", method=RequestMethod.POST)
 	public String saveBook(Book book) {
-		repository.save(book);
+		brepository.save(book);
 		return "redirect:booklist";
 	}
 	
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-		repository.deleteById(bookId);
+		brepository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
 	
 	@RequestMapping(value="/edit/{id}")
-	public String addBook(@PathVariable("id") Long bookId, Model model) {
-		model.addAttribute("book", repository.findById(bookId));
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", brepository.findById(bookId));
 		model.addAttribute("genre", grepository.findAll());
 		return "editbook";
 	}
